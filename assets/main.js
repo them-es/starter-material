45823b1d08735f777f0329606ca3b25b0c80381b
them-es <themes@klicc.at>
2020-01-07 10:25:54 +0100

    WordPress Coding Standards

--- assets/main.js
+++ assets/main.js
@@ -2,21 +2,21 @@
 import * as mdc from 'material-components-web'; // Get all components
 
 
-document.addEventListener('DOMContentLoaded', function () {
-	
+document.addEventListener( 'DOMContentLoaded', function () {
+
 	// Focus Search if Searchform is empty
-	document.querySelector('.search-form').addEventListener('submit', function (e) {
-		var search = document.querySelector('#s');
-		if (search.value.length < 1) {
+	document.querySelector( '.search-form' ).addEventListener( 'submit', function ( e ) {
+		var search = document.querySelector( '#s' );
+		if ( search.value.length < 1 ) {
 			e.preventDefault();
 			search.focus();
 		}
-	});
+	} );
 
 	// Scrollable tab bar menu: https://github.com/material-components/material-components-web/blob/master/demos/tab-scroller.html
-	window.tabBarScroller = new mdc.tabScroller.MDCTabScroller(document.querySelector('#tab-bar-menu'));
+	window.tabBarScroller = new mdc.tabScroller.MDCTabScroller( document.querySelector( '#tab-bar-menu' ) );
 
-});
+} );
 
 // https://material.io/develop/web/components/auto-init
 mdc.autoInit();
