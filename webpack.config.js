7a0686ad2eccca8aee0b9df1e6e8df44adf16ce8
them-es <them-es@users.noreply.github.com>
2020-04-15 09:08:37 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,34 @@
-const path = require( 'path' );
-const webpack = require( 'webpack' );
-
-module.exports = {
-	context: path.resolve( __dirname, 'assets' ),
-	entry: {
-		main: [ './main.js' ],
-	},
-	output: {
-		path: path.resolve( __dirname, 'assets/js' ),
-		filename: '[name].bundle.js',
-	},
-	devtool: 'source-map',
-	watch: true,
-};
+{
+  "name": "themes-starter-material",
+  "version": "2.4.0",
+  "license": "GPL-2.0",
+  "description": "A WordPress Starter Theme built with Material Components.",
+  "homepage": "http://them.es",
+  "author": "them.es",
+  "repository": {
+    "type": "git",
+    "url": "git://github.com/them-es/themes-starter-material.git"
+  },
+  "scripts": {
+    "watch": "webpack -p"
+  },
+  "browserslist": [
+    "last 2 versions"
+  ],
+  "dependencies": {},
+  "devDependencies": {
+    "@babel/core": "^7.8.7",
+    "@babel/preset-env": "^7.8.7",
+    "autoprefixer": "^9.7.0",
+    "babel-loader": "^7.1.5",
+    "css-loader": "^1.0.0",
+    "dart-sass": "^1.24.3",
+    "extract-loader": "^3.0.0",
+    "file-loader": "^2.0.0",
+    "material-components-web": "^5.1.0",
+    "postcss-loader": "^3.0.0",
+    "sass-loader": "^7.1.0",
+    "webpack": "^4.34.0",
+    "webpack-cli": "^3.3.11"
+  }
+}
