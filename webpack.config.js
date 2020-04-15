5985f0d65d6e0a3acfdf0d907a0189fcfe03ea48
them-es <them-es@users.noreply.github.com>
2020-04-15 09:10:10 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,54 @@
-{
-  "name": "themes-starter-material",
-  "version": "2.4.0",
-  "license": "GPL-2.0",
-  "description": "A WordPress Starter Theme built with Material Components.",
-  "homepage": "http://them.es",
-  "author": "them.es",
-  "repository": {
-    "type": "git",
-    "url": "git://github.com/them-es/themes-starter-material.git"
-  },
-  "scripts": {
-    "watch": "webpack -p"
-  },
-  "browserslist": [
-    "last 2 versions"
-  ],
-  "dependencies": {},
-  "devDependencies": {
-    "@babel/core": "^7.8.7",
-    "@babel/preset-env": "^7.8.7",
-    "autoprefixer": "^9.7.0",
-    "babel-loader": "^7.1.5",
-    "css-loader": "^1.0.0",
-    "dart-sass": "^1.24.3",
-    "extract-loader": "^3.0.0",
-    "file-loader": "^2.0.0",
-    "material-components-web": "^5.1.0",
-    "postcss-loader": "^3.0.0",
-    "sass-loader": "^7.1.0",
-    "webpack": "^4.34.0",
-    "webpack-cli": "^3.3.11"
-  }
-}
+const path = require( 'path' );
+const autoprefixer = require( 'autoprefixer' );
+
+// https://github.com/material-components/material-components-web/blob/master/docs/getting-started.md
+
+module.exports = [{
+	context: path.resolve( __dirname, 'assets' ),
+	entry: [ './main.scss', './main.js' ],
+	output: {
+		filename: './main.bundle.js',
+		path: path.resolve( __dirname, 'assets/dist' )
+	},
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: 'main.css',
+						},
+					},
+					{ loader: 'extract-loader' },
+					{ loader: 'css-loader' },
+					{
+						loader: 'postcss-loader',
+						options: {
+							plugins: () => [ autoprefixer() ]
+						}
+					},
+					{
+						loader: 'sass-loader',
+						options: {
+							implementation: require( 'dart-sass' ), // Prefer Dart Sass
+							webpackImporter: false, // See https://github.com/webpack-contrib/sass-loader/issues/804
+							sassOptions: {
+								includePaths: [ './node_modules' ],
+							},
+						},
+					}
+				]
+			},
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				query: {
+					presets: [ '@babel/preset-env' ],
+				},
+			}
+		]
+	},
+	watch: true,
+} ];
