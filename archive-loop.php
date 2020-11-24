c875e84061d4ca2caf54de305e28de66272f0cd5
them-es <themes@klicc.at>
2020-11-24 08:36:19 +0100

    Minor formatting updates
    Tested up to WP v5.6

--- archive-loop.php
+++ archive-loop.php
@@ -5,21 +5,19 @@
 
 themes_starter_content_nav( 'nav-above' );
 
-	/* Start the Loop */
-	if ( have_posts() ) :
-		while ( have_posts() ) :
-			the_post();
-		
-			/**
-			 * Include the Post-Format-specific template for the content.
-			 * If you want to overload this in a child theme then include a file
-			 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-			 */
-			get_template_part( 'content', 'index' ); // Post format: content-index.php
-			
-		endwhile;
-	endif;
-	
-	wp_reset_postdata(); // end of the loop.
+if ( have_posts() ) :
+	while ( have_posts() ) :
+		the_post();
+
+		/**
+		 * Include the Post-Format-specific template for the content.
+		 * If you want to overload this in a child theme then include a file
+		 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+		 */
+		get_template_part( 'content', 'index' ); // Post format: content-index.php
+	endwhile;
+endif;
+
+wp_reset_postdata();
 
 themes_starter_content_nav( 'nav-below' );
--- footer.php
+++ footer.php
@@ -1,23 +1,30 @@
 
 			</div><!-- /.mdc-grid__inner -->
 		</main><!-- /#main -->
-
 		<footer id="footer" class="mdc-layout-grid__cell mdc-layout-grid__cell--span-12">
 			<div class="mdc-layout-grid footer__content">
-				<?php if ( is_active_sidebar( 'secondary_widget_area' ) ) : ?>
+				<?php
+					if ( is_active_sidebar( 'secondary_widget_area' ) ) :
+				?>
 					<div id="widget-area" class="mdc-layout-grid widget-area" role="complementary">
 						<div class="mdc-layout-grid__inner">
 							<?php dynamic_sidebar( 'secondary_widget_area' ); ?>
 						</div>
 
-						<?php if ( current_user_can( 'manage_options' ) ) : ?>
+						<?php
+							if ( current_user_can( 'manage_options' ) ) :
+						?>
 							<span class="edit-link"><a href="<?php echo esc_url( admin_url( 'widgets.php' ) ); ?>" class="badge badge-info"><?php _e( 'Edit', 'my-theme' ); ?></a></span><!-- Show Edit Widget link -->
-						<?php endif; ?>
+						<?php
+							endif;
+						?>
 					</div><!-- /.widget-area -->
-				<?php endif; ?>
+				<?php
+					endif;
+				?>
 				
 				<p class="copyright">
-					<small>&copy; <?php echo date( 'Y' ); ?> <?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?></small>
+					<?php printf( __( '&copy; %1$s %2$s. All rights reserved.', 'my-theme' ), esc_attr( date_i18n( 'Y' ) ), esc_attr( get_bloginfo( 'name', 'display' ) ) ); ?>
 				</p>
 
 				<?php
@@ -33,8 +40,8 @@
 			</div><!-- /.footer-content -->
 		</footer>
 	</div><!-- /#wrapper -->
-
-<?php wp_footer(); ?>
-
+	<?php
+		wp_footer();
+	?>
 </body>
 </html>
