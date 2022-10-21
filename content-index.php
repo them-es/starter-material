43a7f205c4903ae29268c1ebe00a5f7e6a4e7a57
them-es <them-es@users.noreply.github.com>
2022-10-21 12:05:33 +0200

    WP Coding standards

--- content-index.php
+++ content-index.php
@@ -15,7 +15,7 @@
 	<section class="mdc-card__primary">
 		<header class="mdc-card__title">
 			<h2 class="mdc-card__title-text">
-				<a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'my-theme' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
+				<a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'my-theme' ), the_title_attribute( array( 'echo' => false ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
 			</h2>
 		</header><!-- /.entry-header -->
 
@@ -28,28 +28,28 @@
 
 					$num_comments = get_comments_number();
 					if ( comments_open() && $num_comments >= 1 ) :
-						echo ' <a href="' . get_comments_link() . '" class="pull-right mdc-badge" title="' . esc_attr( sprintf( _n( '%s Comment', '%s Comments', $num_comments, 'my-theme' ), $num_comments ) ) . '" data-badge="' . $num_comments . '">' . $num_comments . ' <i class="material-icons">chat_bubble_outline</i></a>';
+						echo ' <a href="' . esc_url( get_comments_link() ) . '" class="pull-right mdc-badge" title="' . esc_attr( sprintf( _n( '%s Comment', '%s Comments', $num_comments, 'my-theme' ), $num_comments ) ) . '" data-badge="' . esc_attr( $num_comments ) . '">' . esc_html( $num_comments ) . ' <i class="material-icons">chat_bubble_outline</i></a>';
 					endif;
 				?>
 			</p>
 		<?php
 			endif;
 
-			if ( has_post_thumbnail() ) :
+			if ( has_post_thumbnail() ) {
 				echo '<div class="post-thumbnail">' . get_the_post_thumbnail( get_the_ID(), 'large' ) . '</div>';
-			endif;
+			}
 
-			if ( is_search() ) :
+			if ( is_search() ) {
 				the_excerpt();
-			else :
+			} else {
 				the_content();
-			endif;
+			}
 
 			wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'my-theme' ) . '</span>', 'after' => '</div>' ) );
 		?>
 	</section>
 
 	<footer class="mdc-card__actions mdc-card--border">
-		<a href="<?php echo get_the_permalink(); ?>" class="mdc-button mdc-button--stroked"><?php esc_html_e( 'more', 'my-theme' ); ?></a>
+		<a href="<?php the_permalink(); ?>" class="mdc-button mdc-button--stroked"><?php esc_html_e( 'more', 'my-theme' ); ?></a>
 	</footer>
 </article><!-- /#post-<?php the_ID(); ?> -->
